68992357920d8c01ca4c14700257b835bcfb40ad
VALENTIN_LOYA__ <a.22308051281244@cbtis128.edu.mx>
2025-02-20 19:34:34 +0000

    creando widgets en columna

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,42 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiWidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiWidgets extends StatelessWidget {
+  const MiWidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Loya",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Color(0xfffd1101),
+          centerTitle: true,
+        ),
+        body: const Column(
+          children: <Widget>[
+            Text(
+              'Valentin te Folla',
+              style: TextStyle(color: Color(0xffff5f14), fontSize: 40),
+            ),
+            Text(
+              'Mat: 22308051281244 Gpo: 6-J',
+              style: TextStyle(color: Color(0xff1900ff), fontSize: 20),
+            ),
+            Expanded(
+              child: FittedBox(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
+    ); //fin de material
+  } //build
+} //mi widgets
